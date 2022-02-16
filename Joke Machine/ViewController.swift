43c51cd9ea6c66c5899c0cdd9523e27f58c1b313
Ogulcan Asa <ogulcanasa@hotmail.com>
2022-02-16 23:58:30 +0300

    Initial Commit

---
+++ Joke Machine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Joke Machine
+//
+//  Created by Oğulcan Aşa on 16.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
